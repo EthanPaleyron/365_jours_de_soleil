aa99cb5aa60b20d13db6adb6882786b03b222218
SoldatdOR05 <epaleyron@edenschool.fr>
2023-06-28 17:58:02 +0200

    fin de l'examen

--- js/main.js
+++ js/main.js
@@ -9,66 +9,66 @@ const newPrevisions = [
         lowerTemp: "14",
     },
     {
-        days: "lun",
-        number: "20",
+        days: "mar",
+        number: "21",
+        sky: "../public/fog.png",
+        higherTemp: "14",
+        lowerTemp: "14",
+    },
+    {
+        days: "mer",
+        number: "22",
+        sky: "../public/fog.png",
+        higherTemp: "14",
+        lowerTemp: "14",
+    },
+    {
+        days: "jeu",
+        number: "23",
+        sky: "../public/fog.png",
+        higherTemp: "14",
+        lowerTemp: "14",
+    },
+    {
+        days: "ven",
+        number: "24",
+        sky: "../public/fog.png",
+        higherTemp: "14",
+        lowerTemp: "14",
+    },
+    {
+        days: "sam",
+        number: "25",
+        sky: "../public/fog.png",
+        higherTemp: "14",
+        lowerTemp: "14",
+    },
+    {
+        days: "dim",
+        number: "26",
         sky: "../public/fog.png",
         higherTemp: "14",
         lowerTemp: "14",
     },
     {
         days: "lun",
-        number: "20",
-        sky: "../public/fog.png",
-        higherTemp: "14",
-        lowerTemp: "14",
-    },
-    {
-        days: "lun",
-        number: "20",
-        sky: "../public/fog.png",
-        higherTemp: "14",
-        lowerTemp: "14",
-    },
-    {
-        days: "lun",
-        number: "20",
-        sky: "../public/fog.png",
-        higherTemp: "14",
-        lowerTemp: "14",
-    },
-    {
-        days: "lun",
-        number: "20",
-        sky: "../public/fog.png",
-        higherTemp: "14",
-        lowerTemp: "14",
-    },
-    {
-        days: "lun",
-        number: "20",
-        sky: "../public/fog.png",
-        higherTemp: "14",
-        lowerTemp: "14",
-    },
-    {
-        days: "lun",
-        number: "20",
+        number: "27",
         sky: "../public/fog.png",
         higherTemp: "14",
         lowerTemp: "14",
     },
 
     {
-        days: "lun",
-        number: "20",
+        days: "mar",
+        number: "28",
         sky: "../public/fog.png",
         higherTemp: "14",
         lowerTemp: "14",
         over_eight: "over_eight",
     },
     {
-        days: "lun",
-        number: "20",
+        days: "mer",
+        number: "29",
         sky: "../public/fog.png",
         higherTemp: "14",
         lowerTemp: "14",
@@ -78,7 +78,7 @@ const newPrevisions = [
 
 for (let article of newPrevisions) {
     articles.innerHTML += `
-    <article class="${article.over_eight}">
+    <article class="previsions ${article.over_eight}">
         <h2>${article.days} <span>${article.number}</span></h2>
         <img src="${article.sky}" alt="prévisions météo">
         <p>
