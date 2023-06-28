fa9c175ac8b6daffc4e6327336769bb1f87c09d4
SoldatdOR05 <epaleyron@edenschool.fr>
2023-06-28 16:36:27 +0200

    petit pause de 5 min

--- js/main.js
+++ js/main.js
@@ -57,11 +57,28 @@ const newPrevisions = [
         higherTemp: "14",
         lowerTemp: "14",
     },
+
+    {
+        days: "lun",
+        number: "20",
+        sky: "../public/fog.png",
+        higherTemp: "14",
+        lowerTemp: "14",
+        over_eight: "over_eight",
+    },
+    {
+        days: "lun",
+        number: "20",
+        sky: "../public/fog.png",
+        higherTemp: "14",
+        lowerTemp: "14",
+        over_eight: "over_eight",
+    },
 ];
 
 for (let article of newPrevisions) {
     articles.innerHTML += `
-    <article>
+    <article class="${article.over_eight}">
         <h2>${article.days} <span>${article.number}</span></h2>
         <img src="${article.sky}" alt="prévisions météo">
         <p>
